bec34a68a44aebd9e09d042d93d309f8ebe7d7da
Jingxiao <35812790+JingxiaoSherry@users.noreply.github.com>
2018-11-22 01:18:50 -0500

    Update tracking error project.py

--- tracking error project.py
+++ tracking error project.py
@@ -19,12 +19,7 @@ from scipy import stats
 from pandas import ExcelWriter
 from pandas import ExcelFile
 from scipy import optimize
-import matplotlib.pyplot as plt
-import statsmodels.tsa.stattools as sts
 
-# weights
-TickerNWeights = pd.read_excel('weight.xlsx', sheet_name='Sheet1', header=0)
-wts_index = TickerNWeights['Weight']
 # download price
 def getDataBatch(tickers, startdate, enddate):
   def getData(ticker):
@@ -33,18 +28,24 @@ def getDataBatch(tickers, startdate, enddate):
   return(pd.concat(datas, keys=tickers, names=['Ticker', 'Date']))
 start_dt = datetime.datetime(2008, 10, 31)
 end_dt = datetime.datetime(2018, 10, 31)
-# tickers = ['ACAD','ALKS','ALNY','ALXN','AMGN','ARRY','BIIB','BMRN','CELG','EXEL','FOLD','GHDX','GILD','HALO','ILMN','IMMU','INCY','IONS','JAZZ','LGND','MDCO','MYGN','MYL','NBIX','NKTR','OPK','REGN','RGEN','SGEN','SHPG','SRPT','TECH','UTHR','VRTX']
-tickers = TickerNWeights['Ticker']
+tickers = ['ACAD','ALKS','ALNY','ALXN','AMGN','ARRY','BIIB','BMRN','CELG','EXEL','FOLD','GHDX','GILD','HALO','ILMN','IMMU','INCY','IONS','JAZZ','LGND','MDCO','MYGN','MYL','NBIX','NKTR','OPK','REGN','RGEN','SGEN','SHPG','SRPT','TECH','UTHR','VRTX']
 stock_data = getDataBatch(tickers, start_dt, end_dt)
 daily_close_px = stock_data.reset_index().pivot(index='Date', columns='Ticker', values='Adj Close')
 # write excel
 daily_close_px.to_csv('TEdata.csv', header=True, index=True)
+# weights
+TickerNWeights = pd.read_excel('weight.xlsx', sheet_name='Sheet1', header=0, index_col=0)
+wts_index = TickerNWeights['Weight']
 # sort tickers by weights
 daily_close_px_transpose = daily_close_px.T
 daily_close_px_transpose.loc[:,'Weight'] = TickerNWeights.loc[:,'Weight']
 daily_close_px_sort = daily_close_px_transpose.sort_values('Weight',ascending=False,axis=0)
-
-daily_close_px_transpose['Weight'].head()
+#daily_close_px_sort['Weight'].head()
+del daily_close_px_sort['Weight']
+daily_close_px2 = daily_close_px_sort.T
+# Calculate returns
+daily_return = daily_close_px2.pct_change().dropna()
+num_periods, num_stock = daily_return.shape
 #-------------------------------Luo------------------------------------------------
 Index_prs = np.ones(2518)
 
@@ -87,6 +88,78 @@ plt.show()
 
 
 #C. Forecast covariance matrix
+# define optimizer function step by step
+def rand_weights(n):
+    ''' Produces n random weights that sum to 1 '''
+    k = np.random.rand(n)
+    return k / sum(k)
+
+def te_opt(W_Bench, C, obj_te, c_, b_):
+    # function that minimize the objective function
+    n = len(W_Bench)
+    # change the initial guess to help test whether we find the global optimal
+    guess = 2
+    #W = rand_weights(n) # start with random weights
+    if guess==1:
+        W = rand_weights(n) # start with random weights
+    elif guess==2:
+        W = W_Bench # Use Bench weight as initial guess
+    else:
+        W = 1/n*np.ones([n,1])
+    
+    optimized = optimize.minimize(obj_te, W, (W_Bench, C), 
+                method='SLSQP', constraints=c_, bounds=b_,  
+                options={'ftol':1e-10, 'maxiter': 1000000, 'disp': False})        
+    if not optimized.success: 
+        raise BaseException(optimized.message)
+    return optimized.x  # Return optimized weights
+
+def obj_te(W, W_Bench, C): 
+    wts_active = W - W_Bench
+    return(np.sqrt(np.transpose(wts_active)@C@wts_active))
+
+def opt_min_te(W, C, b_, c_):
+    return(te_opt(W, C, obj_te, c_, b_))
+    
+# define EWMA covariance
+def ewma_cov(rets, lam): 
+    T, n = rets.shape
+    ret_mat = rets.as_matrix()
+    EWMA = np.zeros((T,n,n))
+    S = np.cov(ret_mat.T)  
+    EWMA[0,:] = S
+    for i in range(1, T) :
+        S = lam * S  + (1-lam) * np.matmul(ret_mat[i-1,:].reshape((-1,1)), 
+                      ret_mat[i-1,:].reshape((1,-1)))
+        EWMA[i,:] = S
+
+    return(EWMA)
+
+# Calulate Covariance Matrix
+   
+def tracking_error(wts_active,cov):
+    TE = np.sqrt(np.transpose(wts_active)@cov@wts_active)
+    return TE
+
+lam = 0.94
+# vol of the assets 
+vols = daily_return.std()
+rets_mean = daily_return.mean()
+# demean the returns
+daily_return = daily_return - rets_mean
+
+# var_ewma calculation by calling ewma_cov function
+var_ewma = ewma_cov(daily_return, lam)
+var_ewma_annual = var_ewma*252 #Annualize
+# take only the covariance matrix for the last date, which is the forecast for next time period
+cov_end = var_ewma[-1,:]
+# Annualize
+cov_end_annual = cov_end*252 
+std_end_annual = np.sqrt(np.diag(cov_end))*np.sqrt(252)
+# calculate the correlation matrix
+corr = daily_return.corr()
+
+
 #1. #MA
 
 
